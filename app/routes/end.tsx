1dee54552ba105c4ed0cdc134ff4a00d09a850c5
= <=>
2024-08-20 06:32:49 -0600

    hide

--- app/routes/end.tsx
+++
@@ -1,9 +0,0 @@
-import { assessmentCompleted } from '~/data/auth.server';
-
-export default function AssessmentEnd() {
-  return null;
-}
-
-export async function loader({ request }: { request: Request }) {
-  return assessmentCompleted(request); // This will log the user out
-}
--- app/routes/logout.js
+++
@@ -1,8 +0,0 @@
-import { destroyUserSession } from '../data/auth.server';
-
-export function action({ request }) {
-  if (request.method !== 'POST') {
-    return new Response('Invalid request method', { status: 405 });
-  }
-  return destroyUserSession(request);
-}
